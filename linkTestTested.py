9c37b9858804d443d05e9ac79898db0736550f28
Sheldon Chen <nh8157@hotmail.com>
2022-12-24 10:10:44 +0000

    Fixed bugs in file comparison

--- linkTestTested.py
+++ linkTestTested.py
@@ -1,6 +1,7 @@
 from pydriller import Repository
 from github import Github
 import os
+import time
 
 """
     STEP 1 
@@ -34,47 +35,57 @@ def find_main_test_dir(repo, path):
     return res
 
 """
-    STEP 2
+    STEP 3
     For each valid directory retrieved from step 2.
     Run breadth first search on both 'main' and 'test' folder
     Check if there exists test file that ends with ClassNameTest.java
 """
 def find_test_tested_pair(repo, path):
     # traverse through all folders and add all files into a set
-    res = []
+    res, paths = [], [path + '/test']
     file_set = set()
-    test_files = repo.get_contents(path + '/test')
-    while len(test_files) > 0:
-        next_level = []
-        for file in test_files:
-            if file.type == 'dir':
-                next_level.append(file)
-            else:
-                file_set.add(file.name)
-        source_files = next_level
-    source_files = repo.get_contents(path + '/main')
-    while len(source_files) > 0:
-        next_level = []
-        for file in source_files:
-            if file.type == 'dir':
-                next_level.append(file)
-            else:
-                if file + 'Test.java' in file_set or file + 'Suite.java' in file_set:
-                    res.append(file.name)
-        source_files = next_level
+    count = 0
+    while len(paths) > 0:
+        new_paths = []
+        for p in paths:
+            test_files = repo.get_contents(p)
+            for file in test_files:
+                if file.type == 'dir':
+                    new_paths.append(file.path)
+                else:
+                    count += 1
+                    file_set.add(file.name)
+                    print("File count:", count)
+
+        paths = new_paths
+    paths = [path + '/main']
+    while len(paths) > 0:
+        new_paths = []
+        for p in paths:
+            source_files = repo.get_contents(p)
+            for file in source_files:
+                if file.type == 'dir':
+                    new_paths.append(file.path)
+                else:
+                    if file.name[:-5] + 'Test.java' in file_set or file.name + 'Suite.java' in file_set:
+                        res.append(file.name)
+                    count += 1
+                    print("File count:", count)
+        paths = new_paths
     return res
 
 if __name__ == '__main__':
-    REPO_NAME = "apache/spark"
+    REPO_NAME = "apache/flink"
     AUTHENTICATOR = os.getenv("AUTHENTICATOR")
-    g = Github(AUTHENTICATOR)
+    g = Github(login_or_token=AUTHENTICATOR, per_page=100)
 
     repo = g.get_repo(REPO_NAME)
     print("Mining repo " + REPO_NAME + '.')
 
     if is_maven_project(repo):
         print("Project is maven.")
-        paths = find_main_test_dir(repo, "")
+        paths = find_main_test_dir(repo, "flink-clients/src")
+        # paths = ["flink-connectors/flink-connector-aws-base/src"]
         print("Located valid directories with /main and /test folders.")
         for path in paths:
             test_tested_pairs = find_test_tested_pair(repo, path)
