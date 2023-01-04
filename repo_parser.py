3da59db9c24452f31b2d44dccd6dd2e0c63800a5
Yuechen Chen <yuechen.chen.22@ucl.ac.uk>
2023-01-04 23:30:19 +0000

    Question 2: correlation between commit size and test results

--- repo_parser.py
+++ repo_parser.py
@@ -5,6 +5,8 @@ from pydriller import ModificationType
 from pydriller import Repository
 
 COMMIT_COUNT = 3000
+SMALL_COMMIT = 10
+LARGE_COMMIT = 100
 
 def is_test_file(file):
     return file.filename.endswith("Test.java")
@@ -24,12 +26,36 @@ def is_maven_project(repo):
             return True
     return False
 
+def parser_commit_size(repo_url):
+    repo = Repository(repo_url)
+    print("Connected to repo")
+
+    small_commit = 0
+    medium_commit = 0
+    large_commit = 0
+
+    ctr = 0
+    for commit in repo.traverse_commits():
+        if commit.lines < SMALL_COMMIT:
+            small_commit += 1
+        elif commit.lines < LARGE_COMMIT:
+            medium_commit += 1
+        else:
+            large_commit += 1
+        ctr += 1
+        # if ctr > COMMIT_COUNT:
+        #     break
+    print("small_commit: %d" % small_commit)
+    print("medium_commit: %d" % medium_commit)
+    print("large_commit: %d" % large_commit)
+
 def parser_main(repo_url):
     repo = Repository(repo_url)
     print("Connected to repo")
 
     total_commit = 0
     commit_with_test_class = 0
+    commits = {}
     test_classes = {}
     tested_classes = {}
     modified_files_sum = 0
@@ -47,6 +73,8 @@ def parser_main(repo_url):
             print("\nUnable to access commit", commit.hash)
             continue
 
+        commits[commit.hash] = (commit.committer_date.timestamp(), commit.lines)
+
         for file in commit.modified_files:
             # change_type == ModificationType.ADDED
             # what if the filename/path has changed?
@@ -56,11 +84,11 @@ def parser_main(repo_url):
                     file_without_extension = file.filename[0:len(file.filename) - 9]
                     has_test_class = True
                     if test_classes.get(file_without_extension) is None:
-                        test_classes[file_without_extension] = commit.committer_date.timestamp()
+                        test_classes[file_without_extension] = commit.hash
                 elif is_tested_file(file):
                     file_without_extension = file.filename[0:len(file.filename) - 5]
                     if tested_classes.get(file_without_extension) is None:
-                        tested_classes[file_without_extension] = commit.committer_date.timestamp()
+                        tested_classes[file_without_extension] = commit.hash
             elif file.change_type == ModificationType.RENAME and file.old_path is not None:
                 # check if the original file exists
                 if is_test_file(file):
@@ -73,7 +101,7 @@ def parser_main(repo_url):
                         test_classes[new_file_without_extension] = test_classes[old_file_without_extension]
                         del test_classes[old_file_without_extension]
                     else:
-                        test_classes[new_file_without_extension] = commit.committer_date.timestamp()
+                        test_classes[new_file_without_extension] = commit.hash
                 elif is_tested_file(file):
                     old_file_name = file.old_path.split('/')[-1]
                     old_file_without_extension = parse_tested_file_name(old_file_name)
@@ -82,7 +110,7 @@ def parser_main(repo_url):
                         test_classes[new_file_without_extension] = test_classes[old_file_without_extension]
                         del test_classes[old_file_without_extension]
                     else:
-                        test_classes[new_file_without_extension] = commit.committer_date.timestamp()
+                        test_classes[new_file_without_extension] = commit.hash
             elif file.change_type == ModificationType.DELETE:
                 if is_test_file(file):
                     file_without_extension = parse_test_file_name(file.filename)
@@ -108,27 +136,43 @@ def parser_main(repo_url):
     print("tested_classes_count: %d" % len(tested_classes))
     print("modified_files_avg: %d, modified_lines_avg: %d" % (modified_files_sum / total_commit, modified_lines_sum / total_commit))
 
-    test_before_count = 0
-    test_same_count = 0
-    test_after_count = 0
+    test_before_count = [0, 0, 0]
+    test_same_count = [0, 0, 0]
+    test_after_count = [0, 0, 0]
     test_without_class = 0
     for test_class_name in test_classes.keys():
         if tested_classes.get(test_class_name) is None:
             test_without_class += 1
         else:
-            t1 = test_classes[test_class_name]
-            t2 = tested_classes[test_class_name]
-            if t1 < t2:
-                test_before_count += 1
-            elif t1 == t2:
-                test_same_count += 1
+            cmt = commits.get(test_classes[test_class_name])
+            # question 2
+            if cmt[1] < SMALL_COMMIT:
+                index = 0
+            elif cmt[1] < LARGE_COMMIT:
+                index = 1
             else:
-                test_after_count += 1
+                index = 2
 
-    print(
-        "test_before_count: %d, test_same_count: %d, test_after_count: %d, test_without_tested_class: %d, tested_class_without_test: %d"
-        % (test_before_count, test_same_count, test_after_count, test_without_class,
-        len(tested_classes) - len(test_classes)))
+            t1 = commits.get(test_classes[test_class_name])[0]
+            t2 = commits.get(tested_classes[test_class_name])[0]
+            if t1 < t2:
+                test_before_count[index] += 1
+            elif t1 == t2:
+                test_same_count[index] += 1
+            else:
+                test_after_count[index] += 1
+
+    print("test_without_tested_class: %d, tested_class_without_test: %d"
+          % (test_without_class, len(tested_classes) - len(test_classes)))
+
+    for i in range(3):
+        print(
+            "commit size: %d, test_before_count: %d, test_same_count: %d, test_after_count: %d"
+            % (i, test_before_count[i], test_same_count[i], test_after_count[i]))
+
+    # for c in commits.keys():
+    #     print("hash: %s, timestamp: %d, lines: %d" % (c, commits.get(c)[0], commits.get(c)[1]))
+
     return (test_before_count, test_same_count, test_after_count)
 
 if __name__ == '__main__':
@@ -148,10 +192,21 @@ if __name__ == '__main__':
             continue
         print("Project is using maven")
 
+        # parser_commit_size(repo_url)
+
         (test_before_count, test_same_count, test_after_count) = parser_main(repo_url)
 
         file_name = repo_name.split('/')[1]
         f = open("data/" + file_name + '.txt', "w")
-        f.writelines(["test_before:" + str(test_before_count) + '\n', "test_same:" + str(test_same_count) + '\n', "test_after:" + str(test_after_count)])
+        f.writelines(["test_before:" + str(test_before_count[0] + test_before_count[1] + test_before_count[2])
+                      + '\n', "test_same:" + str(test_same_count[0] + test_same_count[1] + test_same_count[2])
+                      + '\n', "test_after:" + str(test_after_count[0] + test_after_count[1] + test_after_count[2])
+                      + "\n"])
+        for i in range(3):
+            f.writelines(["commit_size:" + str(i)
+                          + "\ntest_before:" + str(test_before_count[i])
+                          + "\ntest_same:" + str(test_same_count[i])
+                          + "\ntest_after:" + str(test_after_count[i])
+                          + "\n"])
         f.close()
         print()
