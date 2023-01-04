4f20e315785fec42134f456734358d7197e2655f
xiaohanjc00 <xiaohanjc00@gmail.com>
2023-01-04 12:50:52 +0100

    add new search argument

--- repo_parser.py
+++ repo_parser.py
@@ -25,7 +25,7 @@ def is_maven_project(repo):
     return False
 
 def parser_main(repo_url):
-    repo = Repository(repo_url)
+    repo = Repository(repo_url, only_modifications_with_file_types=['.java'])
     print("Connected to repo")
 
     total_commit = 0
