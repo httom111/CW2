d0a5798569727c9aa74600e053db0236e5fc9de8
Sheldon Chen <nh8157@hotmail.com>
2023-01-06 14:45:25 +0000

    Final version

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,7 +1,12 @@
 import os
+import re
 import matplotlib.pyplot as plt
 import numpy as np
 from util import BUCKET_SIZE, BUCKET_COUNT
+
+DARK_BLUE = "#2596be"
+LIGHT_BLUE = "#abdbe3"
+ORANGE = "#e28743"
 
 # Read data from txt and parse it into a dictionary
 def read_data(file):
@@ -29,23 +34,34 @@ def calc_frequency(numerator, denominator):
 
 def draw_stacked_freq(data, path):
     labels, test_before, test_same, test_after = [], [], [], []
-
-    for f, d in data.items():
+    test_before_sum, test_same_sum, test_after_sum = 0, 0, 0
+    keys = sorted(data.keys(), key=lambda x: len(x))
+    for f in keys:
+        d = data[f]
         total = d["test_before"] + d["test_same"] + d["test_after"]
-        labels.append(f.strip(".txt"))
+        labels.append(f[:-4])
+        test_before_sum += d["test_before"]
+        test_same_sum += d["test_same"]
+        test_after_sum += d["test_after"]
         test_before.append(calc_frequency(d["test_before"], total) * 100)
         test_same.append(calc_frequency(d["test_same"], total) * 100)
         test_after.append(calc_frequency(d["test_after"], total) * 100)
+    total = test_before_sum + test_same_sum + test_after_sum
+    print("total = " + str(total))
+    print("test_before = {}\ntest_same = {}\ntest_after = {}".format(test_before_sum, test_same_sum, test_after_sum))
+    print("P(test_before) = {}\nP(test_same) = {}\nP(test_after) = {}".format(test_before_sum/total, test_same_sum/total, test_after_sum/total))
 
     index = np.arange(len(labels))
     bar_width = 0.5
-    plt.figure(figsize=(16, 8))
-    plt.bar(index, test_before, label="Test Before", width=bar_width)
-    plt.bar(index, test_same, bottom=test_before, label="Test Same", width=bar_width)
-    plt.bar(index, test_after, bottom=[test_before[i] + test_same[i] for i in range(len(test_same))], label="Test After", width=bar_width)
-    plt.xticks(index, labels, fontsize=12)
+    plt.figure(figsize=(10, 8))
+    plt.bar(index, test_before, label="Test Before", width=bar_width, color=DARK_BLUE)
+    plt.bar(index, test_same, bottom=test_before, label="Test Same", width=bar_width, color=LIGHT_BLUE)
+    plt.bar(index, test_after, bottom=[test_before[i] + test_same[i] for i in range(len(test_same))], label="Test After", width=bar_width, color=ORANGE)
+    plt.xticks(index, [re.sub("(.{18})", "\\1\n", label, 0, re.DOTALL) for label in labels], fontsize=12, rotation='vertical')
+    plt.yticks([i for i in range(0, 100, 10)], fontsize=12)
     plt.ylabel("Percentage", fontsize=15)
-    plt.legend()
+    plt.legend(fontsize=15)
+    plt.tight_layout()
     plt.savefig(path)
 
 def draw_line_graph(data, path):
@@ -65,14 +81,15 @@ def draw_line_graph(data, path):
         test_after[i] -= max_after
 
     x = [i * BUCKET_SIZE for i in range(BUCKET_COUNT)]
-    plt.figure(figsize=(10, 8))
-    plt.plot(x, test_before, label="Test before")
-    plt.plot(x, test_same, label="Test same")
-    plt.plot(x, test_after, label="Test after")
+    plt.figure(figsize=(10, 5))
+    plt.plot(x, test_before, label="Test before", color=DARK_BLUE)
+    plt.plot(x, test_same, label="Test same", color=LIGHT_BLUE)
+    plt.plot(x, test_after, label="Test after", color=ORANGE)
     plt.xticks(x)
-    plt.xlabel("Lines changed")
-    plt.ylabel("Number of occurrences")
-    plt.legend()
+    plt.xlabel("Lines changed", fontsize=15)
+    plt.ylabel("Number of occurrences", fontsize=15)
+    plt.legend(fontsize=13)
+    plt.tight_layout()
     plt.savefig(path)
 
 # This is only meant for research question 1 now
